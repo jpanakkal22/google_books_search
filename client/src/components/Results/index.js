99ffd8b10177e3789f927d176ff52463d2059a80
New Dad <josh_panakkal3122@hotmail.com>
2020-10-26 01:00:56 -0400

    change card layout

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -2,39 +2,27 @@ import React from "react";
 import "./style.css";
 
 function Results(props){
-    return (
-        <div className="container">
+    return (          
+        <div className="card m-4">
             <div className="row">
-                <div className="col-md-12">
-                <div className="row no-gutters">
-            <div className="col-md-3">
-                <img src={props.image} className="card-img" alt="..." />
-            </div>
-            <div className="col-md-9">
-                <div className="card-body">
-                    <h5 className="card-title">Title: {props.title} <br/> Author: {props.author}</h5>
-                    <p className="card-text">{props.description}</p>
-                    <a
-                target="_blank"
-                href={props.link}
-                rel="noopener noreferrer"
-              >
-                Go to book!
-              </a>
-
-                    
+                <div className="col-md-2">
+                    <img alt="..." className="img-fluid" src={props.image} />
+                </div>
+                <div className="col-md-10">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title} by {props.author}</h5>
+                        <p className="card-text">{props.description}</p>
+                        <div>
+                            <a href={props.link} className="btn badge-pill btn-outline-dark mt-3" target="_blank" >View</a>
+                            {/* <button onClick={() => this.handleSave(result)} className="btn badge-pill btn-outline-warning mt-3 ml-3" >
+                                {this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save"}
+                            </button> */}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-                </div>
-            <div className="card mb-3">
-        
-            </div>
-        </div>
-    
-    
-        
+              
     )
 }
 
