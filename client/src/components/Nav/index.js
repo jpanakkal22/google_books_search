1873bc305e3b26c35f1f96c3904b2b266a102921
New Dad <josh_panakkal3122@hotmail.com>
2020-12-23 18:52:10 -0500

    redo navigation with react-bootstrap and style

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,40 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import "./style.css";
 
-function Nav() {
+function Navigation() {
   return (
-        <nav className="navbar navbar-expand-lg mb-0" id="Navbar">
-        <a className="navbar-brand" href="/" id="navbarBrand">Google Books Search</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active textColor">
-            <Link
-              to="/"
-              className= {
-                window.location.pathname === "/" || window.location.pathname === "/search"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-              >
-              Search
-            </Link>              
-            </li>
-            <li className="nav-item textColor">
-            <Link
-              to="/saved"
-              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-            >
-              Saved
-            </Link>              
-            </li>
-          </ul>
-        </div>
-    </nav>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/" id="brand">Google Books Search</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto" id="links">
+          <Nav.Link href ="/search">SEARCH</Nav.Link>
+          <Nav.Link href ="/saved">SAVED</Nav.Link>     
+        </Nav>        
+      </Navbar.Collapse>
+    </Navbar>    
   );
 }
 
-export default Nav;
+export default Navigation;
