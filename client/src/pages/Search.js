b251a26c65559608becc17c280cb8f960adf9e3f
New Dad <josh_panakkal3122@hotmail.com>
2020-12-23 23:09:57 -0500

    remove old form and add new Input form

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
-import Results from "../components/Results";
+import { Row, Col } from 'react-bootstrap/';
 import Input from "../components/Input";
-import Button from "../components/Button";
+import Results from "../components/Results";
 import API from "../utils/API";
-import { Container, Row, Col } from "../components/Grid";
-
 
 function Search() {
   // Setting our component's initial state
   const [books, setBooks] = useState([])
   const [formInput, setFormInput] = useState("");
 
-  const handleInputChange = event => {
-    
+  // Use form input to set state
+  const handleInputChange = event => {    
     const { value } = event.target;
     setFormInput(value);
   };
@@ -26,57 +24,44 @@ function Search() {
   
   return (
     <div>
-    <Row>
-      <Col size="md-12">
-        <form>
-          <Container>
-            <Row>
-              <Col size="xs-9 sm-10">
-                <Input
-                name="bookSearch"
-                value={formInput}
-                onChange={handleInputChange}
-                placeholder="Search For a Book"
-                />
-              </Col>
-              <Col size="xs-3 sm-2">
-                <Button
-                onClick={handleFormSubmit}
-                className="input-lg"
-                >
-                Search
-                </Button>
-              </Col>
-            </Row>
-          </Container>
-        </form>
-      </Col>
-    </Row>
-    <Row>
-      <Col size="xs-12">
-        {!books.length ? (
-        <h1>No Books to Display</h1>
-        ) : (
-        <div>
-        {books.map(book => {
-        return (
-        <Results 
-          key={book.id} 
-          title={book.volumeInfo.title}
-          author={book.volumeInfo.authors}
-          image={book.volumeInfo.imageLinks.smallThumbnail}
-          description={book.volumeInfo.description}
-          link={book.volumeInfo.infoLink}
-          buttonName="Save"
-        >
-        </Results>)
-        })}
-        </div>      
-        )}
-      </Col>
-    </Row>
-  </div>
-)   
+      <Row className="d-flex justify-content-center">
+        <Col sm={8}>        
+          <Input 
+           name="bookSearch" 
+           value={formInput}
+           onChange={handleInputChange}
+           placeholder="Search by author, title or keyword"
+           onClick={handleFormSubmit}
+          />
+        </Col>
+      </Row>    
+      <Row>
+        <Col size="xs-12">
+          {!books.length ? (
+          <h1>No Books to Display</h1>
+          ) : (
+          <div>
+          {books.map(book => {
+          return (
+          <Results 
+            key={book.id} 
+            title={book.volumeInfo.title}
+            author={book.volumeInfo.authors}
+            image={book.volumeInfo.imageLinks.smallThumbnail}
+            description={book.volumeInfo.description}
+            link={book.volumeInfo.infoLink}
+            buttonName="Save"
+          >
+          </Results>)
+          })}
+          </div>      
+          )}
+        </Col>
+      </Row>
+    </div>
+       
+
+  )   
 }
 
 export default Search;
