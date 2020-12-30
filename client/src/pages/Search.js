f61c5a088857c7777e87d26cbd0993ed4a8c94f9
New Dad <josh_panakkal3122@hotmail.com>
2020-12-29 23:44:15 -0500

    add jumbotron and grid layout for card mapping

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { Row, Col } from 'react-bootstrap/';
+import { Container, Row, Col } from 'react-bootstrap/';
+import Jmbotron from "../components/Jmbotron";
 import Input from "../components/Input";
 import Results from "../components/Results";
 import API from "../utils/API";
+import "./style.css";
 
 function Search() {
   // Setting our component's initial state
@@ -24,43 +26,46 @@ function Search() {
   
   return (
     <div>
-      <Row className="d-flex justify-content-center">
-        <Col sm={8}>        
-          <Input 
-           name="bookSearch" 
-           value={formInput}
-           onChange={handleInputChange}
-           placeholder="Search by author, title or keyword"
-           onClick={handleFormSubmit}
-          />
-        </Col>
-      </Row>    
-      <Row>
-        <Col size="xs-12">
-          {!books.length ? (
-          <h1>No Books to Display</h1>
-          ) : (
-          <div>
-          {books.map(book => {
-          return (
-          <Results 
-            key={book.id} 
-            title={book.volumeInfo.title}
-            author={book.volumeInfo.authors}
-            image={book.volumeInfo.imageLinks.smallThumbnail}
-            description={book.volumeInfo.description}
-            link={book.volumeInfo.infoLink}
-            buttonName="Save"
-          >
-          </Results>)
-          })}
-          </div>      
-          )}
-        </Col>
-      </Row>
-    </div>
-       
-
+      <Jmbotron />
+      <Container>    
+        <Row className="d-flex justify-content-center">
+          <Col sm={8}>        
+            <Input 
+            name="bookSearch" 
+            value={formInput}
+            onChange={handleInputChange}
+            placeholder="Search by author, title or keyword"
+            onClick={handleFormSubmit}
+            />
+          </Col>
+        </Row>      
+        {!books.length ? (
+          <Row className="d-flex justify-content-center">
+            <Col sm={12} className="d-flex justify-content-center">
+            <h2 className="text-center mt-3">No Books to Display</h2>
+            </Col>
+          </Row>
+        ) : (
+          <Row>            
+            {books.map(book => {
+              return (
+                <Col sm={4} className="d-flex justify-content-center">
+                  <Results 
+                    key={book.id} 
+                    title={book.volumeInfo.title}
+                    author={book.volumeInfo.authors}
+                    image={book.volumeInfo.imageLinks.smallThumbnail}
+                    description={book.volumeInfo.description}
+                    link={book.volumeInfo.infoLink}
+                    buttonName="Save"
+                  />
+                </Col>
+              )
+            })}            
+          </Row>          
+        )}        
+      </Container>
+    </div>    
   )   
 }
 
