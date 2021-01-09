ab20e7af1abf4944a9057595afbddf1745bb4a38
New Dad <josh_panakkal3122@hotmail.com>
2021-01-09 11:08:38 -0500

    add container, key prop and style

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import API from "../utils/API";
 import Results from "../components/Results";
 import "./style.css";
@@ -15,38 +15,30 @@ function Saved() {
     // Loads all books and sets them to books
     function loadBooks() {
         API.getBooks()
-        .then(res => {
-            setBooks(res.data)
-            console.log(res.data)
-        })
+        .then(res => setBooks(res.data))
         .catch(err => console.log(err));
     };
 
     return (
-        <div id="saved">           
-            <Row className="d-flex justify-content-center">
-                <Col sm={12}>
-                    <h1 className="text-center" id="savedH1">MY LIBRARY</h1>       
-                </Col>
-            
+        <Container id="saved">  
+            <h1 className="text-center" id="savedH1">MY LIBRARY</h1>           
+            <Row className="d-flex justify-content-center">                        
                 {books.map(book => {
-                    return (
-                        <Col sm={4} className="d-flex justify-content-center">
-                            <Results 
-                            id={book._id}
-                            title={book.title}
-                            author={book.author}
-                            description={book.description}
-                            image={book.image}    
-                            link={book.link}
-                            buttonName="Remove" 
-                            load={loadBooks}               
-                            />
-                        </Col>
+                    return (                    
+                        <Results 
+                        id={book._id}
+                        key={book._id} 
+                        title={book.title}
+                        author={book.author}
+                        image={book.image}    
+                        link={book.link}
+                        buttonName="Remove" 
+                        load={loadBooks}               
+                        />                       
                     )
                 })}                
             </Row>    
-        </div>
+        </Container>
     )
 }
 
